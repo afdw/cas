bbd94cbb727b96b186545bd445d0a908e1f06064
Anton Danilkin <afdw@yandex.ru>
2020-09-27 19:41:38 +0200

    Add Gtk and Cairo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use gio::prelude::*;
+use gtk::{prelude::*, Application, ApplicationWindow, DrawingArea};
+
 fn main() {
-    println!("Hello, world!");
+    let application = Application::new(None, Default::default()).unwrap();
+    application.connect_activate(|application| {
+        let window = ApplicationWindow::new(application);
+        let drawing_area = DrawingArea::new();
+        drawing_area.connect_draw(|_, cr| {
+            cr.move_to(0.0, 10.0);
+            cr.text_path("test");
+            cr.fill();
+            Inhibit(false)
+        });
+        window.add(&drawing_area);
+        window.show_all();
+    });
+    application.run(&[]);
 }
